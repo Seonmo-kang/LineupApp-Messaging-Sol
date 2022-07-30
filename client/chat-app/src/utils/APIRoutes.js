1e8c6741b40f1a64cab46a2c93a483a9ff5365e3
Seonmo <tjsah18@gmail.com>
2022-07-30 11:59:57 -0400

    Initialize the server and client

---
+++ client/chat-app/src/utils/APIRoutes.js
@@ -0,0 +1,7 @@
+export const HOST = process.env.REACT_APP_BASEURL;
+export const registerRoute = `${HOST}/api/auth/register`;
+export const loginRoute = `${HOST}/api/auth/login`;
+export const setAvatarRoute = `${HOST}/api/auth/setAvatar`;
+export const allUsersRoute = `${HOST}/api/auth/allusers`;
+export const addMessageRoute = `${HOST}/api/messages/addMessages`;
+export const getMessageRoute = `${HOST}/api/messages/getMessages`;
